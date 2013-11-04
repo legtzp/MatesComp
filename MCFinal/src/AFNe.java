2816aa5e6f14765421962c3de58bfc1cfdbb1b30
Cesar De la Torre <cesardelatorre3@gmail.com>
2013-11-03 18:59:41 -0600

    Inicio de Implementacion AFNe

---
+++ MCFinal/src/AFNe.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+
+
+public class AFNe {
+	
+	Vector<Vector<String>> automata;
+	
+	public AFNe(String posfijo){
+		this.convertir(posfijo);
+	}
+	
+	public void convertir(String posfijo){
+		int nEstado, inicial1, inicial2, final1, final2;
+		
+		
+	}
+
+}
