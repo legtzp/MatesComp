20427176f5c40d5b0ef5fbfac1093bcdad98f998
Cesar De la Torre <cesardelatorre3@gmail.com>
2013-11-19 16:27:33 -0600

    Arreglado \n al final de cada linea

--- MCFinal/src/modulo4.java
+++ MCFinal/src/modulo4.java
@@ -24,12 +24,13 @@ public class modulo4 {
 		BufferedReader lector =  new BufferedReader(new FileReader(archivo));
 		
 		while((linea = lector.readLine()) != null){ //Lee cada linea individual
+			linea = linea + '\n';
 			for(int i = 0; i < linea.length();i++){ //Crea todas las subcadenas posibles de la linea y prueba si son aceptadas por el automata
 				for(int j = 0; j <= linea.length(); j++){
 					if(i<j){
 						sublinea = linea.substring(i, j);
 						//System.out.println("i: "+i+" j: "+j+" ->"+sublinea);
-						if(automata.aceptado(automata.resolverCadena(sublinea, 0))){
+						if(automata.aceptado(automata.resolverCadena(sublinea, 0)) && !this.cadenasAceptadas.contains(sublinea)){
 							this.cadenasAceptadas.add(sublinea);
 						}
 					}
@@ -46,7 +47,6 @@ public class modulo4 {
 	
 	public String toString(){ //Override del metodo substring que imprime todas las cadenas
 		String string = "";
-		System.out.println(this.cadenasAceptadas);
 		for(int i = 0; i < this.cadenasAceptadas.size(); i++){
 			System.out.println(this.cadenasAceptadas.elementAt(i));
 			string.concat(this.cadenasAceptadas.elementAt(i)+"\n");
@@ -58,12 +58,10 @@ public class modulo4 {
 	
 	public static void main(String[] args) throws ExpresionPosfijaException, IOException{
 		
-		ConvertidorPosfijo posfijo = new ConvertidorPosfijo("(.)*César(.)*");
+		ConvertidorPosfijo posfijo = new ConvertidorPosfijo("S(.)*\n");
 		AFNe afne = new AFNe(posfijo.getPosfijo());
 		AFN afn = new AFN(afne);
-		
-		//afn.imprimirAFN();
-		modulo4 prueba = new modulo4(afn,"prueba2.txt");
+		modulo4 prueba = new modulo4(afn,"prueba3.txt");
 		System.out.println(prueba);
 		
 	}
